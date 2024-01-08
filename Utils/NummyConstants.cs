21335b8ffe9cba417b542e333b12d86b1a3683f2
Hamid Musayev <hemidvsmusayev@gmail.com>
2024-01-08 17:01:57 +0400

    routes fixed

--- Utils/NummyConstants.cs
+++ Utils/NummyConstants.cs
@@ -3,6 +3,6 @@
 internal class NummyConstants
 {
     public const string ClientName = "NummyHttpLoggerClient";
-    public const string RequestLogAddUrl = "/api/log/http/request";
-    public const string ResponseLogAddUrl = "/api/log/http/response";
+    public const string RequestLogAddUrl = "/api/log/request";
+    public const string ResponseLogAddUrl = "/api/log/response";
 }
